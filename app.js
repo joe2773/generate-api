2652f954f02756bf37f1583479145e1f185bd2e5
joe2773 <jbarrymarron@gmail.com>
2023-04-21 22:57:40 +1200

    adding secret manager integration

--- app.js
+++ app.js
@@ -2,32 +2,44 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-
+const AWS = require('aws-sdk');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 require('dotenv').config();
-const API_KEY = process.env.API_KEY;
+
+
+const secretsManager = new AWS.SecretsManager();
+
+
 
 app.post('/generate-image', async (req, res) => {
-  const { prompt, imageSize } = req.body;
+    const { prompt, imageSize } = req.body;
+        const params = {
+    SecretId: 'open-ai-api-key'
+    };
+
+    const secretData =  await secretsManager.getSecretValue(params).promise();
+    const API_KEY = JSON.parse(secretData.SecretString).API_KEY;
+
+    console.log(API_KEY);
 
 
     const response = await axios.post(
         'https://api.openai.com/v1/images/generations',
         {
-        prompt,
-        imageSize,
+            prompt,
+            imageSize,
         },
         {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${API_KEY}`,
-        },
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${API_KEY}`,
+            },
         }
     );
-
     res.json(response.data);
+   
 
 });
 
